0262f6137f8b0359939dc6f730585c539eec4066
dlarbi <dean.m.larbi@gmail.com>
2014-12-05 01:42:09 -0800

    Update dunitjs.js

--- dunitjs.js
+++ dunitjs.js
@@ -2,51 +2,59 @@
 	 'use strict';
 	 var DUnitJS = function() {
 		  
-		 //We create assert objects with a build function to enforce 
+		 this.config = {
+			 alterTitle:false //Change title to 'PASSED TESTS' if passed
+		 };
+		 
+		 //We create testSuite objects with a build function to enforce 
 		 //that each test instance gets its own properties (ie. failure counters).
 		 //Multiple tests can take place at the same time, and their results wont interfere.
-		 var buildAssert = function() {
-			 //The assert object contains all the testing options a user has.
+		 var buildTestSuite = function() {
+			 //The testSuite object contains all the testing options a user has.
 			 //Its properties and methods are only available inside the testRoutine defined by a user.  
-			 var assert = {};
-			 assert.testCount =  assert.failCount =  assert.warningCount = 0;
+			 var testSuite = {};
+			 testSuite.testCount =  testSuite.failCount =  testSuite.warningCount = 0;
 			 
-			 //assert.equal(x,y) usage -> DUNITJS.test('test_name', function() { assert.equal(x,y) });
+			 //testSuite.equal(x,y) usage -> DUNITJS.test('test_name', function() { testSuite.equal(x,y) });
 			 //checks if x and y are equal: 
-			 assert.equal = function(x,y) {			 
+			 testSuite.equal = function(x,y) {			 
 				 if(x === y) {
 					 console.log('PASS: ' + x + ' and ' + y + ' are identical. They are the same object.');
 				 } else if (x == y) {
 					 console.log('WARNING: ' + x + ' and ' + y + ' fail strict comparison.  They are not of the same type.');
-					 assert.warningCount++;
+					 testSuite.warningCount++;
 				 } else {
 					 console.log('FAIL: ' + x + ' is not equal to ' + y);
-					 assert.failCount++;
+					 testSuite.failCount++;
 				 }
-				 assert.testCount++;			 
+				 testSuite.testCount++;			 
 			 };
 			 
-			 assert.notEqual = function(x, y) {
+			 testSuite.notEqual = function(x, y) {
 				 if(x != y) {
 					 console.log('PASS: ' + x + ' is not equal to ' + y);
 				 } else {
 					 console.log('FAIL: ' + x + ' and ' + y + ' are equal.');
-					 assert.failCount++;
+					 testSuite.failCount++;
 				 }				 
-				 assert.testCount++;			 
+				 testSuite.testCount++;			 
 			 };
-			 return assert;
-		 };		 
+			 return testSuite;
+		 };
+		 
+		 
 		 
 		 //DUNITJS.test(test_name, testRoutine)
-		 //We build a new assert object and pass it to to the testRoutine function.
-		 //The testRoutine function is defined by the user in their implementation of DUNITJS.test().
-		 //The assert object and its tests are available to the user inside of DUNITJS.test()'s callback.
-		 this.test = function(testName, testRoutine) {	
-			 var assert = buildAssert();
+		 //We build a new testSuite object and pass it to to the testRoutine function.
+		 //The testRoutine function is implemented by the user in DUNITJS.test().
+		 //The testSuite object and its tests are available to the user inside of DUNITJS.test()'s callback.
+		 this.test = function(testName, testRoutine) {			
+			 var testSuite = buildTestSuite();
 			 console.log('You are running: ' + testName);
-			 testRoutine(assert);
-			 $(document).trigger('testComplete', {testName: testName, total: assert.testCount, fails:assert.failCount, warnings: assert.warningCount, pass: assert.testCount - assert.failCount});
+			 testRoutine(testSuite);
+			 //If config.alterTitle is true we alert the user of results on their test's tab.
+			 this.config.alterTitle && testSuite.failCount === 0 ? document.title = 'PASSED ALL TESTS' : document.title = 'FAILED ' + testSuite.failCount;
+			 $(document).trigger('testComplete', {testName: testName, total: testSuite.testCount, fails:testSuite.failCount, warnings: testSuite.warningCount, pass: testSuite.testCount - testSuite.failCount});
 		 };
 		 
 		 //The test results are available in this callback
@@ -54,16 +62,15 @@
 		 //The callback will only fire if the done method's argument
 		 //matches the name of the test that triggered the event.
 		 this.done = function(testName, doneCallback) {
-			 $(document).on('testComplete', function(e, results) {
+			 $(document).off('testComplete').on('testComplete', function(e, results) {
 				 if(testName == results.testName) {
 					 doneCallback(results);
 				 }				 
 			 });
-		 };
+		 };		 
 		 
 	 };
 	 	 
 	 window.DUNITJS = new DUnitJS();
 	 return;
 })(jQuery);
-
