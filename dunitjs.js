97017adb7e764c051242c5605233b622e7085634
dlarbi <dean.m.larbi@gmail.com>
2014-12-05 02:23:09 -0800

    Update dunitjs.js

--- dunitjs.js
+++ dunitjs.js
@@ -9,7 +9,7 @@
 		 //We create testSuite objects with a build function to enforce 
 		 //that each test instance gets its own properties (ie. failure counters).
 		 //Multiple tests can take place at the same time, and their results wont interfere.
-		 var buildTestSuite = function() {
+		 var buildTestSuite = function(testName) {
 			 //The testSuite object contains all the testing options a user has.
 			 //Its properties and methods are only available inside the testRoutine defined by a user.  
 			 var testSuite = {};
@@ -27,7 +27,8 @@
 					 console.log('FAIL: ' + x + ' is not equal to ' + y);
 					 testSuite.failCount++;
 				 }
-				 testSuite.testCount++;			 
+				 testSuite.testCount++;
+				 $(document).trigger('assertComplete', {testName: testName, arg1:x, arg2:y, total: testSuite.testCount, fails:testSuite.failCount, warnings: testSuite.warningCount, pass: testSuite.testCount - testSuite.failCount})
 			 };
 			 
 			 testSuite.notEqual = function(x, y) {
@@ -37,19 +38,19 @@
 					 console.log('FAIL: ' + x + ' and ' + y + ' are equal.');
 					 testSuite.failCount++;
 				 }				 
-				 testSuite.testCount++;			 
+				 testSuite.testCount++;
+				 $(document).trigger('assertComplete', {testName: testName, arg1:x, arg2:y, total: testSuite.testCount, fails:testSuite.failCount, warnings: testSuite.warningCount, pass: testSuite.testCount - testSuite.failCount})
 			 };
 			 return testSuite;
 		 };
 		 
-		 
-		 
+		 	 
 		 //DUNITJS.test(test_name, testRoutine)
 		 //We build a new testSuite object and pass it to to the testRoutine function.
 		 //The testRoutine function is implemented by the user in DUNITJS.test().
 		 //The testSuite object and its tests are available to the user inside of DUNITJS.test()'s callback.
 		 this.test = function(testName, testRoutine) {			
-			 var testSuite = buildTestSuite();
+			 var testSuite = buildTestSuite(testName);
 			 console.log('You are running: ' + testName);
 			 testRoutine(testSuite);
 			 //If config.alterTitle is true we alert the user of results on their test's tab.
@@ -67,7 +68,19 @@
 					 doneCallback(results);
 				 }				 
 			 });
-		 };		 
+		 };
+		 
+		 //If the user uses DUNITJS.log(), The log callback function 
+		 //will be triggered each time a testSuite.test() is executed.
+		 //Results for the individual test are available. User could write them 
+		 //in a log or store them in a database to track more micro test details.
+		 this.log = function(testName, logCallback) {
+			 $(document).off('assertComplete').on('assertComplete', function(e, results) {		
+				 if(testName == results.testName) {
+					 logCallback(results);
+				 }	
+			 });
+		 };
 		 
 	 };
 	 	 
